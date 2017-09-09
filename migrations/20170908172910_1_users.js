72c3188d244314537d1c0c3096aead9c51f1e8b8
Reid Delahunt <reidpierredelahunt@gmail.com>
2017-09-08 17:37:10 -0700

    reconfigured migration & seed files

--- migrations/20170908172910_1_users.js
+++ migrations/20170908172910_1_users.js
@@ -1,14 +1,15 @@
-exports.up = function (knex) {
+
+exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', (table) => {
     table.increments();
     table.string('spotify_id').unique().notNullable();
     table.string('images').notNullable().defaultTo('');
-    
-    // table.specificType('hashed_password', 'char(60)').notNullable();
+    table.string('email').notNullable().defaultTo('');
+    table.string('display_name').notNullable().defaultTo('');
     table.timestamps(true, true);
   });
 };
 
-exports.down = function (knex) {
+exports.down = function(knex, Promise) {
   return knex.schema.dropTable('users');
 };
