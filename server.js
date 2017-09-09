743bf91155080c574178e50c499922607bed521a
Reid Delahunt <reidpierredelahunt@gmail.com>
2017-09-09 15:48:06 -0700

    fixes

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ passport.use(
       }).then(response => {
         console.log(response);
       })
+      res.redirect('localhost:3000/?code=' + profile);
       // newUser = {
       //   spotify_id: profile.id,
       //   images: JSON.stringify(profile.images),
