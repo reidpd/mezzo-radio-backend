84e0f164a8c1ce0164f06e99de9bc6bf384abe81
Reid Delahunt <reidpierredelahunt@gmail.com>
2017-09-11 17:50:18 -0700

    redirect

--- server.js
+++ server.js
@@ -119,7 +119,7 @@ app.get('/callback', (req, res) => {
       });
 
       // we can also pass the token to the browser to make requests from there
-      const url = 'localhost:3000/interface/?access_token=' + access_token + '&refresh_token=' + refresh_token;
+      const url = 'localhost:3000/interface/' + access_token + '/' + refresh_token;
       res.redirect(url);
       console.log(url)
       console.log('this should never get here');
