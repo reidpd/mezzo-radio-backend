b82980e38ace1648d28af72a5318353153552b40
Reid Delahunt <reidpierredelahunt@gmail.com>
2017-09-16 22:51:24 -0700

    added read-playback-state scope to authorization procedure

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const spotifyApi = new Spotify(credentials);
 
 const STATE_KEY = 'spotify_auth_state';
 // your application requests authorization
-const scopes = ['user-read-private', 'user-read-email'];
+const scopes = ['user-read-private', 'user-read-email', 'user-read-playback-state'];
 
 /** Generates a random string containing numbers and letters of N characters */
 const generateRandomString = N => (Math.random().toString(36)+Array(N).join('0')).slice(2, N+2);
